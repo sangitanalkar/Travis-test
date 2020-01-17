b1e3e497afcecb5c7e0fc00ea8d5525f4e1e278b
Namrata Bhave <Namrata.Bhave@ibm.com>
2020-01-17 14:36:09 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-arch=$(uname -i)
-if [[ "$arch" == "s390x"]] || [[ "$arch" == "ppc64le" ]]; then
+arch="$(uname -i)"
+if [[ $arch == s390x ]] || [[ $arch == ppc64le ]]; then
         echo "i am in s390x or ppc - testing travis"
 else
         echo "i am in else - testing travis"
