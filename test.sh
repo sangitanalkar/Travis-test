2f48a1b0b02e30140fb57cd665d2ec3503fc4be9
sangitanalkar <40229345+sangitanalkar@users.noreply.github.com>
2020-01-17 16:12:55 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ set -xe
 
 arch="$(uname -i)"
 
-if [[ "$arch" == "s390x" ]] || [[ "$arch" == ppc64le ]]; then
+if [[ $(uname -i) != ("ppc64le" || "s390x") ]]; then
         echo "i am in s390x or ppc - testing travis"
 else
         echo "i am in else - testing travis"
