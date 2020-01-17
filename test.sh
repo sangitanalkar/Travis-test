e9205e775df4e68859aaf5de2b9c8bc860182687
Namrata Bhave <Namrata.Bhave@ibm.com>
2020-01-17 12:47:26 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 set -e
-if [[ $(uname -i) == "s390x" ||  "ppc64le" ]]; then
+if [[ $(uname -i) == "s390x" ||  $(uname -i) == "ppc64le" ]]; then
         echo "i am in if - testing travis"
 else
         echo "i am in else - testing travis"
